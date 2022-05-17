b85f4c11340db9fc3b7175504b95b3331818660d
Jose <64102720+Jose-Rodolfo-Llave@users.noreply.github.com>
2022-05-16 20:15:47 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -15,7 +15,31 @@ public class Exercise1 {
 	}
 
 	public boolean esCuadradoPerfecto(int numero) {
-
+		import java.util.*;
+public class prueba2 {
+	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			int n = sc.nextInt();
+			System.out.println(esCuadradoPerfecto(n));			
+		}
+	}
+	
+	static boolean esCuadradoPerfecto(int numero) {
+		double num = numero;
+		num = Math.sqrt(num);
+		if((num % 1 != 0) && contador >= 1) {
+			return true;
+		} else if(num % 1 == 0) {
+			numero = (int)num;
+			contador++;
+			esCuadradoPerfecto(numero);
+		}
 		return false;
+	}
+	
+	static int contador = 0;
+}
+
 	}
 }
